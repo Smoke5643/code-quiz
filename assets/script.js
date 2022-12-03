46a6ec3bb49c6c46689b6b74bbc085166ff39ca1
Smoke5643 <smoke5643@gmail.com>
2022-12-03 15:50:21 -0500

    added classes for questions in HTML and updated Javascript

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,25 @@
-var questions = document.querySelectorAll(".question")
+var questions = document.querySelectorAll(".question");
+var cursor = 0;
 
 var showQuestion = function (){
     for (var question of questions){
-        if (questions.dataset.index != cursor)
+      if (questions.dataset.index != cursor)
         question.style.display = "none";
         
     }
 }
  
-  var advance = function() {
-    if (cursor < questions.length - 1) {
-      cursor++;
+  var advance = function(event) {
+    var element = event.event;
+    
+    if (element.matches('question')){
+      if (cursor < questions.length - 1) {
+        cursor++;
     }
+  }
     showQuestion();
   };
+
+  document.addEventListener('click', advance);
+
+  showQuestion();
